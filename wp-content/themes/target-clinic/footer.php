cf078e76a88d4174aaf0c453f42af8e89b455aaf
swaty007 <swaty0007@gmail.com>
2020-02-18 00:14:49 +0200

    map axx

--- wp-content/themes/target-clinic/footer.php
+++ wp-content/themes/target-clinic/footer.php
@@ -79,6 +79,9 @@
 
 </a>
 		<?php wp_footer(); ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
         crossorigin="anonymous"></script>
